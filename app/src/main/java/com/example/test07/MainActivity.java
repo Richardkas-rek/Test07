2c8dc4f7b74fa6f5cb0113e86fc840ea577abf68
Richardkas-rek <richardlkas161210@gmail.com>
2026-01-25 11:47:45 +0200

    java and finale version

--- app/src/main/java/com/example/test07/MainActivity.java
+++ app/src/main/java/com/example/test07/MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.test07;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.os.Bundle;
 import android.widget.Button;
@@ -13,13 +15,28 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private ImageView iVone;
 
-    private ImageView btniv;
+    private ImageButton btniv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         iVone = findViewById(R.id.iVone);
         btniv = findViewById(R.id.btniv);
+    }
+    int x = 1;
+
+    public void clicked(View view) {
+        x = (int) (Math.random() * 3 + 1);
+        if (x == 1) {
+            iVone.setImageResource(R.drawable.pip);
+            btniv.setImageResource(R.drawable.lil1);
+        } else if (x == 2) {
+            iVone.setImageResource(R.drawable.dog);
+            btniv.setImageResource(R.drawable.lil2);
+        } else {
+            iVone.setImageResource(R.drawable.sum);
+            btniv.setImageResource(R.drawable.lil3);
+        }
     }
 
 }
