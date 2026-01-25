9d017e9de91c35760d9ef47af2ae54d81757ceed
Richardkas-rek <richardlkas161210@gmail.com>
2026-01-25 11:40:57 +0200

    image and xml

--- app/src/main/java/com/example/test07/MainActivity.java
+++ app/src/main/java/com/example/test07/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.test07;
-
+import android.widget.ImageView;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iVone;
 
+    private ImageView btniv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iVone = findViewById(R.id.iVone);
+        btniv = findViewById(R.id.btniv);
     }
+
 }
